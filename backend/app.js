fa1687499d0a5c11bc976c9148d3005ca64a649b
mfurqanabbas20 <you@example.com>
2025-04-20 17:43:38 +0500

    Adding new endpoint

--- backend/app.js
+++ backend/app.js
@@ -69,8 +69,9 @@ app.use('/api/conversation', conversationRouter)
 // message route
 app.use('/api/message', messageRouter)
 
+
 app.get('/', (req, res) => {
-    return res.status(200).send("Hello World")
+    res.json("Hellow")
 })
 
 // starting the server
